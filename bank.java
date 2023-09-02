18971b24c88e73511133b7e680be377b79844871
sam <baconsamp@gmail.com>
2023-09-02 11:10:39 -0700

    Withdraw addded
    :D

--- bank.java
+++ bank.java
@@ -38,6 +38,9 @@ public class bank {
     }
 
     public double depositMoney(String userId, double deposit) throws Exception {
+        if (deposit == 0)
+            throw new Exception("fr? you can't but nothing into nothing. broke b!");
+
         if (ids.containsKey(userId))
             throw new Exception(
                     "This account does not exist brokie. broke boy. get your money up. you're so silly lmaoo im laughing at your and im just code brokie mcbroke face. I can see you wiping your tears but with tissue paper instead of dollar bills. funny.");
@@ -45,11 +48,23 @@ public class bank {
         return deposit;
     }
 
-    public void transferMoney() {
-
+    public boolean transferMoney(String fromAccount, String toAccount, double amount) throws Exception {
+        if(!ids.containsKey(toAccount) || !ids.containsKey(fromAccount))
+        {
+            throw new Exception()
+        }
     }
 
     public void audit() {
 
+    }
+
+    public double withdrawMoney(String userId, double amount) throws Exception {
+        if (ids.containsKey(userId))
+            throw new Exception("This account does not exist brokie.");
+        else if (ids.get(userId) < amount)
+            throw new Exception("BROKE AF. You do not have that much money :)!!!!");
+        ids.put(userId, (ids.get(userId) - amount));
+        return amount;
     }
 }
