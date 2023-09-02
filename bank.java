3559239ad281321ff0df7cbc024caf0469d40676
sam <baconsamp@gmail.com>
2023-09-02 11:02:48 -0700

    Added deposit method
    Also changed all emails to userId (lame)

--- bank.java
+++ bank.java
@@ -11,34 +11,38 @@ public class bank {
         ids = new HashMap<String, Double>();
     }
 
-    public void openAccount(String email, double initBal) throws Exception {
-        if (!ids.containsKey(email)) {
-            ids.put(email, initBal);
+    public void openAccount(String userId, double initBal) throws Exception {
+        if (!ids.containsKey(userId)) {
+            ids.put(userId, initBal);
         } else {
             throw new Exception("You already registered an account!");
         }
     }
 
-    public void closeAccount(String email) throws Exception {
-        if (!ids.containsKey(email))
+    public void closeAccount(String userId) throws Exception {
+        if (!ids.containsKey(userId))
             throw new Exception("This account does not exist");
 
-        if (ids.get(email) != 0)
+        if (ids.get(userId) != 0)
             throw new Exception("There is still an outstanidng balance on this account ");
-        ids.remove(email);
+        ids.remove(userId);
 
     }
 
-    public double checkBalance(String email) throws Exception {
-        if (!ids.containsKey(email))
+    public double checkBalance(String userId) throws Exception {
+        if (!ids.containsKey(userId))
             throw new Exception("this account does not exist");
         else
-            return ids.get(email);
+            return ids.get(userId);
 
     }
 
-    public void depositMoney() {
-
+    public double depositMoney(String userId, double deposit) throws Exception {
+        if (ids.containsKey(userId))
+            throw new Exception(
+                    "This account does not exist brokie. broke boy. get your money up. you're so silly lmaoo im laughing at your and im just code brokie mcbroke face. I can see you wiping your tears but with tissue paper instead of dollar bills. funny.");
+        ids.put(userId, (ids.get(userId) + deposit));
+        return deposit;
     }
 
     public void transferMoney() {
